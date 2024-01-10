18e2022f74cf6eb40b6d41d2afd423af85fca231
Can Kartal <can.krtl05@gmail.com>
2024-01-10 11:01:13 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri Eklendi : " + customer.name + " " + customer.surname);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silindi : " + customer.name + " " + customer.surname);
+        }
+        public void CustomerList(Customer[] customers) 
+        {
+            Console.WriteLine("Müşteriler Listelendi : ");
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.name + " " + customer.surname);
+            }
+        }  
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.name = "Can";
+            customer1.surname = "Kartal";
+            customer1.tcNumber = "3123124124";
+
+            Customer customer2 = new Customer();
+            customer2.name = "Ahmet";
+            customer2.surname = "Uçar";
+            customer2.tcNumber = "12451252124";
+
+            Customer customer3 = new Customer();
+            customer3.name = "Cansu";
+            customer3.surname = "Yıldız";
+            customer3.tcNumber = "215437345234";
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3};
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer1);
+            customerManager.Delete(customer2);
+            customerManager.CustomerList(customers);
+
+        }
+    }
+}
